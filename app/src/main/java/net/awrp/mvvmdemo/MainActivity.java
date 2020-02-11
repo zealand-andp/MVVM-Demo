39c07a0e0a946fa5ad2768189ab086c23292771f
Anders Petersen <AndP@easj.dk>
2020-02-11 11:52:13 +0100

    onClick functionality added to enterButton. ID's refactored/renamed to be more descriptive.

--- app/src/main/java/net/awrp/mvvmdemo/MainActivity.java
+++ app/src/main/java/net/awrp/mvvmdemo/MainActivity.java
@@ -3,6 +3,9 @@ package net.awrp.mvvmdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void enterInput(View view) {
+        EditText inputText = (EditText) findViewById(R.id.inputText);
+        TextView outputView = (TextView) findViewById(R.id.outputView);
+
+        String input = inputText.getText().toString();
+        outputView.setText(input);
     }
 }
