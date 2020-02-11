b75e5207d14a03fe44cc10cb5d450ef8173e5116
Anders Petersen <AndP@easj.dk>
2020-02-11 12:36:06 +0100

    AndroidView updated to use Model class for data storage. onCreate now initialises with data from Model. enterInput now uses Model to store and retrieve data.

--- app/src/main/java/net/awrp/mvvmdemo/view/AndroidView.java
+++ app/src/main/java/net/awrp/mvvmdemo/view/AndroidView.java
@@ -8,14 +8,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import net.awrp.mvvmdemo.R;
+import net.awrp.mvvmdemo.model.Model;
 
 public class AndroidView extends AppCompatActivity {
+
+    private Model model = new Model();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView outputView = (TextView) findViewById(R.id.outputView);
+        outputView.setText(model.getData());
+
+        EditText inputText = (EditText) findViewById(R.id.inputText);
+        inputText.setText(model.getData());
     }
 
     public void enterInput(View view) {
@@ -23,6 +31,7 @@ public class AndroidView extends AppCompatActivity {
         TextView outputView = (TextView) findViewById(R.id.outputView);
 
         String input = inputText.getText().toString();
-        outputView.setText(input);
+        model.setData(input);
+        outputView.setText(model.getData());
     }
 }
